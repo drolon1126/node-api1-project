253fd7733242b8f5141c9d697253ceb32380b113
danRolon <daniel.rolon@yahoo.com>
2019-09-30 13:42:31 -0500

    add GET requests

--- index.js
+++ index.js
@@ -14,6 +14,33 @@ server.use(express.json());
         res.json({message:'The api is working'});
       });
 
+      server.get('/api/users', (req,res)=>{
+        Users
+          .find()
+          .then(users=>{
+            res.status(200).json(users);
+          })
+          .catch(()=>{
+            res.status(500).json({errorMessage:'The users information could not be retrieved.'});
+          });
+      });
+      
+      server.get('/api/users/:id', (req,res)=>{
+        Users
+          .findById(req.params.id)
+          .then(user=>{
+            if(user){
+              res.status(200).json(user);
+            } else {
+              res.status(404).json({errorMessage:'The user with the specified ID does not exist.'});
+            }
+          
+          })
+          .catch(()=>{
+            res.status(500).json({errorMessage:'The user information could not be retrieved.'});
+          });
+      });
+
 /***************************************
  * POST REQUEST
  **************************************/
@@ -21,13 +48,14 @@ server.post('/api/users',(req,res)=>{
   if(!req.body.name || !req.body.bio){
     res.status(400).json({ errorMessage: 'Please provide name and bio for the user.' });
   } else {
-    Users.insert(req.body)
-    .then(user=>{
-      res.status(201).json(user);
-    })
-    .catch(()=>{
-      res.status(500).json({errorMessage:'There was an error while saving the user to the database'});
-    });
+    Users
+      .insert(req.body)
+      .then(user=>{
+        res.status(201).json(user);
+      })
+      .catch(()=>{
+        res.status(500).json({errorMessage:'There was an error while saving the user to the database'});
+      });
   }
 
 });
